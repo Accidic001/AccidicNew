658c474ce104ead7a5af819fd65ab661b80f755c
unknown <muhammadabubakarjamiu@gmail.com>
2024-07-12 01:39:37 -0700

    update the css of the newsData

--- src/assets/NewsData.jsx
+++ src/assets/NewsData.jsx
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom'
 function NewsData(val, key) {
  
   return (
-      <div className='News container'>
+      <div className='News container-fluid'>
             <h3 className=' text-center title pt-4'>{val.title}</h3>
+            <div className="img-container">
             <img src={val.image} alt="url"  />
+            </div>
+            
             <button className='btn btn-primary' href= {val.url} rel='noopener' target='_blank'  > read more...</button>
             <div className="data mt-3">
              <p>Author : {val.source.name}</p>
